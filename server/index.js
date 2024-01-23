588f12d9d26dc3d39d5e8bd3b002f8eb6700473e
Revan Kurniawan <rvnkrwn@gmail.com>
2024-01-23 12:33:10 +0700

    🏠 setup: initialization

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("implementation of auth system with jwt and cookies");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running and up at http://localhost:${PORT}`);
+});
