3a410275edc276e6bcc1fbeb4bc6751d07af5bd3
Revan Kurniawan <rvnkrwn@gmail.com>
2024-01-23 12:40:20 +0700

    🏄 update(endpoint): add route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { getProfile, login, logout, refreshAccessToken } from './controllers/authControllers.js';
+import refreshTokenMiddleware from './middlewares/refreshTokenMiddleware.js';
+import authMiddleware from './middlewares/authMiddleware.js';
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +16,18 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send("implementation of auth system with jwt and cookies");
 });
+
+// login
+app.post('/login', login);
+
+// refresh accessToken by refreshToken
+app.get('/refresh', refreshTokenMiddleware, refreshAccessToken);
+
+// was loggedIn
+app.get('/profile', authMiddleware, getProfile)
+
+// logout
+app.get('/logout', authMiddleware, logout)
 
 app.listen(PORT, () => {
     console.log(`Server running and up at http://localhost:${PORT}`);
